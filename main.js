c8f4ccce5ccdef5dc02305649b33014bbba3dba4
Sarah Lopez <slopez323@gmail.com>
2022-03-15 13:40:53 -0400

    event listeners

--- main.js
+++ main.js
@@ -17,3 +17,37 @@ const dinosaur1 = document.querySelector('#triceratops');
 dinosaur1.style.width = '324px';
 
 //event listener
+messWithMeSpan.addEventListener('click', function(){
+    messWithMeSpan.style.color = 'orange';
+});
+
+dinosaur1.addEventListener('click', function(){
+    dinosaur1.style.border = '1px solid red';
+});
+
+const dinosaur4 = document.querySelector('#feathers');
+
+dinosaur4.addEventListener('click', function(){
+    dinosaur4.style.opacity = '50%';
+});
+
+const switchBC = document.querySelector('#toggle');
+const row = document.querySelector('#row');
+
+switchBC.addEventListener('click', function(){
+    if(row.style.backgroundColor === ''){
+        row.style.backgroundColor = 'yellow';
+    } else {
+        row.style.backgroundColor = '';
+    };
+});
+
+const dinosaur3 = document.querySelector('#biggify');
+
+dinosaur3.addEventListener('mouseover', function(){
+    dinosaur3.style.width = '200px';
+});
+
+dinosaur3.addEventListener('mouseout', function(){
+    dinosaur3.style.width = '';
+});
