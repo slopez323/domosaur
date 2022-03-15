ffe15169ff648f988a21efb46147b43670ec72fc
Sarah Lopez <slopez323@gmail.com>
2022-03-15 12:59:53 -0400

    warmup probs

---
+++ main.js
@@ -0,0 +1,19 @@
+//warmup
+
+const messWithMeSpan = document.querySelector('span.mess-with-me');
+
+messWithMeSpan.style.fontSize = '40px';
+
+const messWithMeP = document.querySelector('p.mess-with-me');
+
+messWithMeP.style.backgroundColor = 'green';
+
+const dinosaur2 = document.querySelector('#hide-me-area');
+
+dinosaur2.style.display = 'none';
+
+const dinosaur1 = document.querySelector('#triceratops');
+
+dinosaur1.style.width = '324px';
+
+//event listener
